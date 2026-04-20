6a067776d55d20c58b62d51c6d899291346cffc6
Meower <bertenev0586@gmail.com>
2026-04-21 00:09:06 +0300

    Implement Google Fonts and set default app typography

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.compose.material3)
 
     implementation(libs.androidx.navigation.compose)
@@ -64,6 +65,7 @@ dependencies {
     implementation(libs.coil.compose)
 
     implementation(libs.timber)
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
